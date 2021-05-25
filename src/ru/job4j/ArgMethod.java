68aa06c6d6ed06b3f38804ff6088bcd5d1d04ae5
konstantingelfand <k.ermakov34@gmail.com>
2021-05-25 16:51:07 +0300

    4.1. Аргументы [#295521]

--- src/ru/job4j/ArgMethod.java
+++ src/ru/job4j/ArgMethod.java
@@ -9,10 +9,6 @@ public class ArgMethod {
         System.out.println(age);
     }
 
-    public static void hello(int age) {
-        System.out.println(age);
-    }
-
     public static void main(String[] args) {
         String name = "Petr Arsentev";
         int age = 33;
