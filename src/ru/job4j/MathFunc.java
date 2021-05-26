43367a41f9b8d0155b04efa7e371204473408336
konstantingelfand <k.ermakov34@gmail.com>
2021-05-26 17:04:08 +0300

    5. Конвертер валюты [#41583]

--- src/ru/job4j/MathFunc.java
+++ src/ru/job4j/MathFunc.java
@@ -2,8 +2,8 @@ package ru.job4j;
 
 public class MathFunc {
     public static int func1(int x) {
-        int y = x * x + 1;
-        return y;
+        return x * x + 1;
+
     }
 
     public static void main(String[] args) {
