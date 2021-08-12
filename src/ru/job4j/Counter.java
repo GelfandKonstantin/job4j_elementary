3c48520b9bddc9a459d96d684a0d60bb821d41bb
konstantingelfand <k.ermakov34@gmail.com>
2021-08-12 12:04:53 +0300

    5.0.1. Сумма чисел. [#226416]

---
+++ src/ru/job4j/Counter.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum;
+        for (sum = 0;  sum <= 10; sum++) {
+            sum = start + finish;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
+}
