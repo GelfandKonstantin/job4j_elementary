3bb20d42b490f487e11fb479b77205d2a58c3423
konstantingelfand <k.ermakov34@gmail.com>
2021-08-22 13:42:16 +0300

    5.4. Протеиновая диета [#156313]

--- src/ru/job4j/Fitness.java
+++ src/ru/job4j/Fitness.java
@@ -3,13 +3,9 @@ package ru.job4j;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (month < 0) {
-            month = ivan * 2;
-            month += 2;
-            System.out.println(month);
-            month = nik * 3;
-            month += 3;
-            System.out.println(month);
+        while (ivan > nik) {
+            ivan = ivan * 3;
+            month += 0;
         }
         return month;
     }
