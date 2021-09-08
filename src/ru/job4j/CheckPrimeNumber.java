bf51a565482fe9ee0743c8d15be40871f86c12e2
konstantingelfand <k.ermakov34@gmail.com>
2021-09-08 09:50:07 +0300

    5.5. Простое число [#171694]

---
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        return prime;
+        for (int index = -1; index < 2; index++); {
+            if (number % index == 0) {
+                break;
+            }
+        }
+    }
+}
